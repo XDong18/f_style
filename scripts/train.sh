0232c1fa0101317b21bb3f8900367166809844a1
Xudong Liu <xudongliu@XudongdeMacBook-Pro.local>
2020-11-24 17:02:00 +0800

    cc

--- scripts/train.sh
+++ scripts/train.sh
@@ -1,5 +1,5 @@
 export PYTHONPATH="${PYTHONPATH}:/shared/xudongliu/code/f_style/lib"
-export CUDA_VISIBLE_DEVICES=2,3,4,5
+export CUDA_VISIBLE_DEVICES=0,1,6,7
 python3 segment.py train -d /shared/xudongliu/bdd100k/seg/seg -c 19 -s 768 --arch dla34up \
     --batch-size 8 --lr 0.01 --momentum 0.9 --lr-mode poly \
     --epochs 500 --bn-sync --random-scale 2 --random-rotate 0 \
