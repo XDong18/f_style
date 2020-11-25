d6423b4050876dc537c3c183bcf297dc1c7bbc72
Xudong Liu <xudongliu@XudongdeMacBook-Pro.local>
2020-11-25 10:57:45 +0800

    cc

--- scripts/eval.sh
+++ scripts/eval.sh
@@ -3,4 +3,4 @@ export CUDA_VISIBLE_DEVICES=0,1,2,3
 python segment.py train -e -d /shared/xudongliu/bdd100k/seg/seg -c 19 -s 768 --arch dla34up \
     --batch-size 8 --lr 0.01 --momentum 0.9 --lr-mode poly \
     --epochs 500 --bn-sync --random-scale 2 --random-rotate 0 \
-    --random-color --pretrained-base imagenet -resume out/segnet_500e/model_best.pth.tar
+    --random-color --pretrained-base imagenet --resume out/segnet_500e/model_best.pth.tar
