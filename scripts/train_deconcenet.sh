4fc7539b2eb2165a5d46e01cf3252b2922ce3a3e
Xudong Liu <xudongliu@XudongdeMacBook-Pro.local>
2020-12-02 01:54:18 +0800

    cc

--- scripts/train_deconcenet.sh
+++ scripts/train_deconcenet.sh
@@ -3,4 +3,4 @@ export CUDA_VISIBLE_DEVICES=0,1,2,3
 python segment_deconvnet.py train -d /shared/xudongliu/bdd100k/seg/seg -c 19 -s 769 --arch dla34up \
     --batch-size 8 --lr 0.01 --momentum 0.9 --lr-mode poly \
     --epochs 500 --bn-sync --random-scale 2 --random-rotate 0 \
-    --random-color --pretrained-base imagenet -o out/convnet_224_500e 
+    --random-color --pretrained-base imagenet -o out/convnet_224-769_500e 
