0f9eac2062ff079625b2bffb70adbf66864ebe47
Xudong Liu <xudongliu@XudongdeMacBook-Pro.local>
2020-12-02 09:48:14 +0800

    cc

--- scripts/eval.sh
+++ scripts/eval.sh
@@ -1,6 +1,6 @@
 export PYTHONPATH="${PYTHONPATH}:/shared/xudongliu/code/f_style/lib"
-export CUDA_VISIBLE_DEVICES=0,1,2,3
+export CUDA_VISIBLE_DEVICES=4,5,6,7
 python segment.py train -e -d /shared/xudongliu/bdd100k/seg/seg -c 19 -s 768 --arch dla34up \
     --batch-size 8 --lr 0.01 --momentum 0.9 --lr-mode poly \
     --epochs 500 --bn-sync --random-scale 2 --random-rotate 0 \
-    --random-color --pretrained-base imagenet -o out/segnet_500e --resume out/segnet_500e/checkpoint_73.pth.tar
+    --random-color --pretrained-base imagenet -o out/segnet_new_500e --resume out/segnet_new_500e/model_best.pth.tar
